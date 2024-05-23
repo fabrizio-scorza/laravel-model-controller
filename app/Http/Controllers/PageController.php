e1055e34df8bdd4f7c52f336ff02b48aabb15335
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-23 17:21:57 +0200

    FEAT pages and db request

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    //funzione per la vista home
+    public function home()
+    {
+        return view('homepage');
+    }
+    public function movies()
+    {
+        // collego dal model movie la tabella movies in una variabile
+        $movies = Movie::all();
+
+        // ritorno la vista e i dati
+        return view('movies', compact('movies'));
+    }
 }
